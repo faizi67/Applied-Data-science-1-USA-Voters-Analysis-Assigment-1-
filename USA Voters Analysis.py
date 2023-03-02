570c535eb9d8e34cc63a8ec596d2a5f75a2c49a6
faizi67 <126816810+faizi67@users.noreply.github.com>
2023-03-02 20:16:11 +0000

    Update USA Voters Analysis.py

--- USA Voters Analysis.py
+++ USA Voters Analysis.py
@@ -27,3 +27,26 @@ plt.subplot(121)
 df.gender.value_counts().plot(kind="pie")
 plt.title("Distribution of Voters amoung Gender")
 plt.ylabel("")
+
+plt.subplot(122)
+df.race.value_counts().plot(kind="pie")
+plt.title("Distribution of Voters amoung Race")
+plt.ylabel("")
+plt.show()
+
+
+# # Visualization 2
+
+# In[24]:
+
+
+plt.figure(figsize=(12, 4))
+plt.subplot(121)
+df.educ.value_counts().plot(kind="bar")
+plt.title("Distribution of Education")
+
+plt.subplot(122)
+df.voter_category.value_counts().plot(kind="bar")
+plt.title("Distribution of Voters")
+plt.ylabel("")
+plt.show()
